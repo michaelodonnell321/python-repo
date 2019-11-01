c91092b07e74b2b23d0a2eca17643b62ccdd8757
Michael O'Donnell <gorillaejones@gmail.com>
2019-11-01 11:43:23 -0500

    numbers, math, modulos etc

--- pythontest.py
+++ pythontest.py
@@ -1,5 +1,26 @@
 print('Hello, World!')
 
+#single line comments are a # symbol
+""" multi line comments are surrounded by 3 " symbols
+"""
+#numbers
+12
+#do some math
+print(1+1)
+print(5-2)
+print(12*12)
+print(111/11)
+
+#integer division with 2 /
+print(7//2) # is 3
+print(14//5) # is 2
+#normal division will give decimals
+print(8.0/3)
+#modulo, same as JS
+print(11%3) # will be 2
+
+
+
 # print('Interest Calculator:')
 # amount = float(input('Principal amount ?'))
 # roi = float(input('Rate of Interest ?'))
